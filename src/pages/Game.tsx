b0a9112ec10f86a958a9710d232218ddd771b9e5
Živorad Milekić <zivoradmilekic@gmail.com>
2019-11-29 12:00:21 +0100

    Added Game component + PWA settings

---
+++ src/pages/Game.tsx
@@ -0,0 +1,168 @@
+import React from 'react';
+
+import {
+  IonButton,
+  IonGrid,
+  IonRow,
+  IonCol,
+  IonContent,
+  IonFooter,
+  IonList,
+  IonLabel,
+  IonItem,
+  IonCard,
+  IonCardHeader,
+  IonCardSubtitle,
+  IonCardTitle,
+  IonNote,
+  IonPage,
+  IonHeader,
+  IonToolbar,
+  IonTitle,
+  IonBadge
+} from '@ionic/react';
+
+const Game: React.FC = () => {
+  const [level, setLevel] = React.useState(0);
+  const [misses, setMisses] = React.useState(0);
+  const [hits, setHits] = React.useState(0);
+
+  const [players, setPlayers] = React.useState({
+    // asdf: {
+    //   name: "a",
+    //   hits: [5, 3, 2, 1]
+    // },
+    // qwer: {
+    //   name: "q",
+    //   hits: [5, 1, 2, 1]
+    // },
+    // sdfg: {
+    //   name: "s",
+    //   hits: [5, 11, 2, 1]
+    // },
+    // erty: {
+    //   name: "e",
+    //   hits: [6, 5, 2, 1]
+    // },
+    // tyui: {
+    //   name: "t",
+    //   hits: [5, 333, 2, 5]
+    // }
+  });
+
+  const sets = [10, 5, 3, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1];
+
+  const _handleScore = () => {
+    const name:any = prompt(`Game over! Enter player name`);
+    let newPlayers:any = {
+      ...players
+    };
+
+    let oldHits = !!newPlayers[name] ? newPlayers[name].hits : [];
+
+    newPlayers[name] = {
+      name,
+      hits: [...oldHits, hits]
+    };
+
+    setPlayers(newPlayers);
+    setMisses(0);
+    setHits(0);
+  };
+
+  const _sortPlayers = (curr:any, prev:any) => {
+    let currHitsStr = curr.hits.toString();
+    let prevHitsStr = prev.hits.toString();
+
+    return currHitsStr < prevHitsStr ? 1 : currHitsStr > prevHitsStr ? -1 : 0;
+  };
+
+  React.useEffect(() => {
+    console.log("Misses", misses);
+    if (misses === sets[level]) {
+      _handleScore();
+    }
+  }, [misses]);
+
+  const playersDisplay = !!Object.keys(players).length && (
+    <IonCard>
+      <IonCardHeader>
+        <IonCardSubtitle>Scoreboard</IonCardSubtitle>
+        <IonCardTitle>Players</IonCardTitle>
+      </IonCardHeader>
+      <IonList inset={true} lines={"full"}>
+        {Object.values(players)
+          .sort((a, b) => _sortPlayers(a, b))
+          .map((player:any, index) => (
+            <IonItem key={index}>
+              <IonLabel>
+                {index + 1} {player.name}
+              </IonLabel>
+              <IonNote slot="end">
+                {player.hits.map((hit:any) => (
+                  <IonBadge color="primary" style={{ marginLeft: "0.5rem" }}>
+                    {hit}
+                  </IonBadge>
+                ))}
+              </IonNote>
+            </IonItem>
+          ))}
+      </IonList>
+    </IonCard>
+  );
+
+  return (
+    <IonPage>
+      <IonHeader>
+        <IonToolbar>
+          <IonTitle>Game</IonTitle>
+        </IonToolbar>
+      </IonHeader>
+      <IonContent scrollEvents={true}>
+        {playersDisplay}
+        <IonGrid className="ion-padding">
+          <IonRow>
+            <IonCol style={{ textAlign: "center" }}>
+              <IonTitle>{sets[level]} misses allowed</IonTitle>
+            </IonCol>
+          </IonRow>
+          <IonRow>
+            <IonCol style={{ textAlign: "center" }}>
+              <IonButton color="warning" onClick={() => setLevel(level + 1)}>
+                Level up ({level + 1})
+              </IonButton>
+            </IonCol>
+          </IonRow>
+        </IonGrid>
+      </IonContent>
+      <IonFooter>
+        <IonGrid>
+          <IonRow>
+            <IonCol style={{ textAlign: "center" }}>
+              <IonNote>{misses}</IonNote>
+              <IonButton
+                color="danger"
+                expand="block"
+                onClick={() => setMisses(misses + 1)}
+              >
+                Miss
+              </IonButton>
+            </IonCol>
+            <IonCol style={{ textAlign: "center" }}>
+              <IonNote>{hits}</IonNote>
+              <IonButton
+                color="success"
+                expand="block"
+                onClick={() => setHits(hits + 1)}
+              >
+                Hit
+              </IonButton>
+            </IonCol>
+          </IonRow>
+        </IonGrid>
+      </IonFooter>
+    </IonPage>
+  );
+}
+
+export default Game;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,23 +1,43 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
 import React from 'react';
+
+import {
+  IonContent,
+  IonHeader,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+  IonGrid,
+  IonRow,
+  IonCol,
+  IonButton,
+  IonText
+} from '@ionic/react';
 
 const Home: React.FC = () => {
   return (
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Ionic Blank</IonTitle>
+          <IonTitle>Office Basket</IonTitle>
         </IonToolbar>
       </IonHeader>
-      <IonContent className="ion-padding">
-        The world is your oyster.
-        <p>
-          If you get lost, the{' '}
-          <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/">
-            docs
-          </a>{' '}
-          will be your guide.
-        </p>
+      <IonContent>
+        <IonGrid className="ion-padding">
+          <IonRow>
+            <IonCol style={{ textAlign: "center" }}>
+              <IonText>
+                Have a fun! Take a break of your hard work!
+              </IonText>
+            </IonCol>
+          </IonRow>
+          <IonRow>
+            <IonCol style={{ textAlign: "center" }}>
+              <IonButton routerLink="/game">
+                Play new game
+              </IonButton>
+            </IonCol>
+          </IonRow>
+        </IonGrid>
       </IonContent>
     </IonPage>
   );
