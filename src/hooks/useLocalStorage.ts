d25a3eff33125d9bdbb9e4807f87999f7f5e2836
Živorad Milekić <zivoradmilekic@gmail.com>
2020-01-03 14:09:05 +0100

    Formated useLocalStorage

--- src/hooks/useLocalStorage.ts
+++ src/hooks/useLocalStorage.ts
@@ -1,17 +1,26 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState } from "react";
 
 type Dispatch<A> = (value: A) => void;
 type SetStateAction<S> = S | ((prevState: S) => S);
 
-const useLocalStorage = <T>(key: string, initialValue?: T, raw?: boolean): [T, Dispatch<SetStateAction<T>>] => {
+const useLocalStorage = <T>(
+  key: string,
+  initialValue?: T,
+  raw?: boolean
+): [T, Dispatch<SetStateAction<T>>] => {
   const [state, setState] = useState<T>(() => {
     try {
       const localStorageValue = localStorage.getItem(key);
-      if (typeof localStorageValue !== 'string') {
-        localStorage.setItem(key, raw ? String(initialValue) : JSON.stringify(initialValue));
+      if (typeof localStorageValue !== "string") {
+        localStorage.setItem(
+          key,
+          raw ? String(initialValue) : JSON.stringify(initialValue)
+        );
         return initialValue;
       } else {
-        return raw ? localStorageValue : JSON.parse(localStorageValue || 'null');
+        return raw
+          ? localStorageValue
+          : JSON.parse(localStorageValue || "null");
       }
     } catch {
       // If user is in private mode or has storage restriction
